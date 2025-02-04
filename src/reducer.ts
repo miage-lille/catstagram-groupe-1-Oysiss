6d2c79f1475a5904730a63200dba870ebedd33b3
Simon Lagneau <simon.lagneau.etu@univ-lille.fr>
2025-02-04 17:02:18 +0100

    doing pictures

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,18 +1,31 @@
 import { Loop, liftState } from 'redux-loop';
 import { compose } from 'redux';
 import { Actions } from './types/actions.type';
+import { Picture } from './types/picture.type';
+import data from './fake-datas.json';
 
-export type State = unknown; // TODO : Update this type !
+export type State = {
+  counter: number
+  pictures: Picture[]
+}; // TODO : Update this type !
 
-export const defaultState = {}; // TODO : Update this value !
+const INIT_PICTURE_NUMBER = 3;
+
+export const defaultState = {
+  counter: INIT_PICTURE_NUMBER,
+  pictures: [data[0], data[1], data[2]]
+}; // TODO : Update this value !
 
 export const reducer = (state: State | undefined, action: Actions): State | Loop<State> => {
   if (!state) return defaultState; // mandatory by redux
   switch (action.type) {
     case 'INCREMENT':
-      throw 'Not Implemented';
+      const addCounter = state.counter + 1;
+      return { ...state, counter: addCounter, pictures: [...state.pictures,data[addCounter-1]] };
     case 'DECREMENT':
-      throw 'Not Implemented';
+      if (state.counter <= 3) return state;
+      const decCounter = state.counter - 1;
+      return { ...state, counter: decCounter, pictures: data.slice(0, decCounter) as Picture[] };
     case 'SELECT_PICTURE':
       throw 'Not Implemented';
     case 'CLOSE_MODAL':
@@ -27,10 +40,10 @@ export const reducer = (state: State | undefined, action: Actions): State | Loop
 };
 
 export const counterSelector = (state: State) => {
-  throw 'Not Implemented';
+  return state.counter;
 };
 export const picturesSelector = (state: State) => {
-  throw 'Not Implemented';
+  return state.pictures;
 };
 export const getSelectedPicture = (state: State) => {
   throw 'Not Implemented';
