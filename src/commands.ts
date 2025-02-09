d110e48c52dbc636927d0ece0cc75a3c0023c717
Simon Lagneau <simon.lagneau.etu@univ-lille.fr>
2025-02-09 23:23:27 +0100

    Now mapping response data with pictures type

--- src/commands.ts
+++ src/commands.ts
@@ -7,8 +7,16 @@ export const cmdFetch = (action: FetchCatsRequest) =>
     () => {
       return fetch(action.path, {
         method: action.method,
-      }).then(checkStatus);
-    },
+      })
+      .then(checkStatus)
+      .then(response => response.json())
+      .then(data => data.hits.map((hit: any) => ({
+        previewFormat: hit.previewURL,
+        webFormat: hit.webformatURL,
+        largeFormat: hit.largeImageURL,
+        author: hit.user
+      })));
+  },
     {
       successActionCreator: fetchCatsCommit, // (equals to (payload) => fetchCatsCommit(payload))
       failActionCreator: fetchCatsRollback, // (equals to (error) => fetchCatsCommit(error))
