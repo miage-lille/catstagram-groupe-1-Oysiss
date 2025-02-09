854bde7d886ddb107551fbf3c06843a5cf46a86f
Simon Lagneau <simon.lagneau.etu@univ-lille.fr>
2025-02-09 23:33:35 +0100

    cmdFetch now uses an explicit promise

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,7 @@
 import { Cmd } from 'redux-loop';
+import { FetchCatsRequest, } from './types/actions.type';
 import { fetchCatsCommit, fetchCatsRollback } from './actions';
-import { FetchCatsRequest } from './types/actions.type';
+import { Picture } from './types/picture.type';
 
 export const cmdFetch = (action: FetchCatsRequest) =>
   Cmd.run(
@@ -8,22 +9,33 @@ export const cmdFetch = (action: FetchCatsRequest) =>
       return fetch(action.path, {
         method: action.method,
       })
-      .then(checkStatus)
-      .then(response => response.json())
-      .then(data => data.hits.map((hit: any) => ({
-        previewFormat: hit.previewURL,
-        webFormat: hit.webformatURL,
-        largeFormat: hit.largeImageURL,
-        author: hit.user
-      })));
-  },
+        .then(checkStatus)
+        .then(response => response.json())
+        .then(parsePictures);
+    },
     {
       successActionCreator: fetchCatsCommit, // (equals to (payload) => fetchCatsCommit(payload))
-      failActionCreator: fetchCatsRollback, // (equals to (error) => fetchCatsCommit(error))
+      failActionCreator: fetchCatsRollback, // (equals to (error) => fetchCatsRollback(error))
     },
   );
 
 const checkStatus = (response: Response) => {
   if (response.ok) return response;
   throw new Error(response.statusText);
+};
+
+const parsePictures = (data: any): Promise<Picture[]> => {
+  return new Promise((resolve, reject) => {
+    try {
+      const pictures: Picture[] = data.hits.map((hit: any) => ({
+        previewFormat: hit.previewURL,
+        webFormat: hit.webformatURL,
+        largeFormat: hit.largeImageURL,
+        author: hit.user
+      }));
+      resolve(pictures);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
